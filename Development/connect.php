c5fb18db6b59d3418197168612981ab514fc0dcf
Ankith Reddy <ankithreddy2001@gmail.com>
2022-04-15 21:56:21 +0530

    Delete Development directory

--- Development/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "root", "billingsoftware") or die($mysqli_error($conn));
-if(!isset($_SESSION))
-{
-    session_start();
-}
-?>
--- Development/creds.py
+++
@@ -1,4 +0,0 @@
-email = "ankithreddy2001@gmail.com" #Replace with your email
-passwd = "okitsok"				#Type in your passwd
-#***#
-NotHSCourse = "Language and Society" #Fill in the HS course you're NOT taking part in
